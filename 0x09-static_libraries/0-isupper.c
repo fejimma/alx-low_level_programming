3a0e1028c51c2d3e15418cfb8d4d88646695cf37
fejimma <fejimma665@gmail.com>
2023-05-17 04:42:19 -0700

    task1 correction

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,4 +1,3 @@
-		
 #include "main.h"
 /**
  * _isupper - uppercase letters
@@ -11,5 +10,5 @@ int _isupper(int c)
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
-	return (0);
+		return (0);
 }
--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,14 +1,14 @@
 #include "main.h"
+#include <stddef.h>
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
- */
+* _strchr - Entry point
+* @s: input
+* @c: input
+* Return: Always 0 (Success)
+*/
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-
 
 	for (; s[i] >= '\0'; i++)
 	{
@@ -17,34 +17,3 @@ char *_strchr(char *s, char c)
 	}
 	return (0);
 }
-
-
-
-
- 
-============================
- 
-2-strlen.c
-
-
-#include "main.h"
-/**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-int _strlen(char *s)
-{
-	int longi = 0;
-
-
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-
-
-	return (longi);
-}
-
